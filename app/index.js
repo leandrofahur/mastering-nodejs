1c3221013b5c56ee63c4c3802666bd53a85f9b38
leandrofahur <fahurleandro@gmail.com>
2021-02-16 18:55:03 -0800

    RESTful API with simple http methods.

---
+++ app/index.js
@@ -0,0 +1,28 @@
+/*
+ * Primary file for the API
+ *
+ */
+
+const http = require("http");
+const url = require("url");
+
+// Server should respond to all requests with a string:
+const server = http.createServer(function (req, res) {
+  // Get the url and parse it:
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the path from the url:
+  var path = parsedUrl.pathname; // untrimmed path that the user requested
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Send the response:
+  res.end("Hello World\n");
+
+  // Log what path the person was asking for:
+  console.log("Request received on path: " + trimmedPath);
+});
+
+// Start server, and have it listen on port 3000
+server.listen(3000, function () {
+  console.log("Server listening on port 3000");
+});
